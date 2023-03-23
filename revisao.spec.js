6cf490dc8be54d0df8397106a6e2b131e2b3a547
Victornf98 <vitinho.98@hotmail.com>
2023-03-22 23:33:23 -0300

    Upload do arquivo de revisao

---
+++ revisao.spec.js
@@ -0,0 +1,49 @@
+//Important concepts:
+
+//Software Testing:
+//It's the process of running a software in order to observe its behavior with respect to necessary requirements, or running a program to intentionally "cause" errors.
+
+
+//Software Testing Steps:
+
+//Planning:
+//-Designing a mind map to organize the requirements (business rules)
+//-Strategies
+//-Testing techniques (functional, structural, and mutation)
+//-Criteria
+//-Features to be tested
+//-Associated risks
+//-Schedule
+
+//Test case design:
+//-Ordered pair - input and expected output
+//-Creating test cases (regressive, positive, negative, functional)
+//-Writing scenarios in Gherkin (syntax to illustrate ideas by describing a behavior (Given/When/Then))
+//-Input data and actions for executing the tests
+//-Steps for executing the test cases
+
+//Software execution:
+//-Run the test cases previously created
+//-Chronological details
+//-If there is any problem in the execution, report what happened in a document.
+
+//Results Analysis:
+//-Software behavior
+//-Quality Indicators (data, charts, reports)
+
+
+//Levels of Software Testing:
+//-Unit testing
+//-Integration testing
+//-System testing
+
+
+//Difference between error, defect, fault, and failure:
+
+//Error = a human action that produces an incorrect result;
+//Defect = an incorrect step, process, or data definition in a software product;
+//Fault = difference between the calculated (or experimental) value and the theoretical value;
+//Failure = the inability of the system or component to perform the required function.
+
+//In summary:
+//An error introduces a defect into the software. When activated, the defect may create a fault. If propagated to the software output, the fault causes a failure.
